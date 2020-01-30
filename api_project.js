5f64cd45571f78db404f27afd70c7c48ea73906d
Joao Miguel Araujo <araujoj2@MACC02X43VWJGH5.local>
2020-01-30 13:21:44 +0000

    first commit

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "LRM API",
+  "version": "0.1.0",
+  "description": "Local Rent Manager API",
+  "title": "Local Rent Manager API",
+  "url": "http://local-rent-manager.github.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-01-29T22:51:10.279Z",
+    "url": "http://apidocjs.com",
+    "version": "0.20.0"
+  }
+});
